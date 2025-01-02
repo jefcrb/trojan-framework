74136456a8f1328552d7bf6e878d8c171ad7b9d4
jefcrb <jef@ssicc.be>
2025-01-02 22:24:51 +0100

    Update

--- modules/ddos_module.py
+++ modules/ddos_module.py
@@ -1,15 +1,24 @@
 import time
 import os
 
-def ping_google():
-    print("Starting Google Ping Module...")
+def ping_target(url, ping_interval):
+    print(f"begin ping module naar {url} met interval van {ping_interval} seconden...")
     while True:
-        response = os.system("ping -c 1 google.com" if os.name != "nt" else "ping -n 1 google.com")
+        response = os.system(f"ping -c 1 {url}" if os.name != "nt" else f"ping -n 1 {url}")
         if response == 0:
-            print("Ping to google.com succeeded.")
+            print(f"ping naar {url} succesvol")
         else:
-            print("Ping to google.com failed.")
-        time.sleep(5)
+            print(f"ping naar {url} gefaald")
+        time.sleep(ping_interval)
 
 if __name__ == "__main__":
-    ping_google()
+    url = "google.com" # default
+    ping_interval = 5
+
+    try:
+        url = globals().get("url", url)
+        ping_interval = globals().get("ping_interval", ping_interval)
+    except Exception as e:
+        print(f"Error: {e}")
+
+    ping_target(url, ping_interval)
